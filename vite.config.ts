38cea900c618b2b7b34311915df450bdab79d5c8
Amanda Siebeneichler <amanda.sieb10@gmail.com>
2026-08-30 17:04:00 -0300

    perf: deploy github pages3

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/sainf-2026/',
   plugins: [react()],
+  build: {
+    outDir: 'docs',
+  },
   resolve: {
     alias: {
       '@': '/src',
